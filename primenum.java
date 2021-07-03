e94402b57f4241057276d558b731ccfcfc962ff6
sknithin06 <nithinsk939@gmail.com>
2021-07-04 03:01:27 +0530

    Enter a value

--- primenum.java
+++ primenum.java
@@ -1,6 +1,21 @@
 package Java;
+import java.util.Scanner;
 public class primenum {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Prime Factor");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a number:");  
+		int n = sc.nextInt();
+		
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			while (n % i == 0) {
+				System.out .println("prime factor of the given number is:"+i);
+				n = n / i;
+			}
+		}
+		if ( n > 2) {
+			System.out .println("prime factor of the given number is:"+n);
+		}
 	}
 }
